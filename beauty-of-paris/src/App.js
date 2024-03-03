77d3c6bf37da501c00b61394a39b2718e335c689
deepaksaini2218 <sainimonti17@gmail.com>
2024-03-03 22:15:44 +0530

    instattlatiom

---
+++ beauty-of-paris/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Test from './MyComponents/Test';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>welcome to beauty of paris</h1>
+      <Test></Test>
+    </div>
+  );
+}
+
+export default App;
---
+++ beauty-of-paris/src/MyComponents/Test.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./Test.css"
+
+function Test(props) {
+    return (
+        <div>
+            <h2>hi i am a test component on this i am also using test css</h2>
+            <button className='btn btn-danger'>hi ritli</button>
+        </div>
+    );
+}
+
+export default Test;
